d7ff709aba0d2d6533b0044f83db4329b117beef
Sebastián Ramírez <tiangolo@gmail.com>
2020-06-05 15:40:11 +0200

    ✨ Add support for handling individual issues on issue events

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 import logging
-from typing import Dict, List, Optional
 from pathlib import Path
+from typing import Dict, List, Optional
 
 from github import Github
 from github.Issue import Issue
@@ -23,6 +23,15 @@ class Settings(BaseSettings):
     github_repository: str
     input_token: SecretStr
     github_event_path: Path
+    github_event_name: Optional[str] = None
+
+
+class PartialGitHubEventIssue(BaseModel):
+    number: int
+
+
+class PartialGitHubEvent(BaseModel):
+    issue: Optional[PartialGitHubEventIssue] = None
 
 
 def get_last_comment(issue: Issue) -> Optional[IssueComment]:
@@ -88,8 +97,8 @@ def process_issue(*, issue: Issue, settings: Settings, owner: NamedUser) -> None
             )
             if last_comment and last_comment.created_at > keyword_event.created_at:
                 logging.info(
-                    f'Not closing as the last comment was written after adding the '
-                    'label: "{keyword}"'
+                    f"Not closing as the last comment was written after adding the "
+                    f'label: "{keyword}"'
                 )
                 if keyword_meta.remove_label:
                     logging.info(f'Removing label: "{keyword}"')
@@ -106,7 +115,7 @@ def process_issue(*, issue: Issue, settings: Settings, owner: NamedUser) -> None
         ):
             logging.info(
                 f'Last comment by user: "{last_comment.user.login}" had HTML keyword '
-                'comment: "{keyword}" and there\'s a closable delay.'
+                f'comment: "{keyword}" and there\'s a closable delay.'
             )
             close_issue(issue=issue, keyword_meta=keyword_meta)
             break
@@ -119,19 +128,19 @@ if __name__ == "__main__":
     g = Github(settings.input_token.get_secret_value())
     repo = g.get_repo(settings.github_repository)
     owner: NamedUser = repo.owner
-    
-    # TODO: remove this
-    import os
-    import pprint
-    pprint.pprint(os.environ)
-    print(f"{settings.github_event_path} exists: {settings.github_event_path.exists()} is_file: {settings.github_event_path.is_file()}")
+    github_event: Optional[PartialGitHubEvent] = None
     if settings.github_event_path.is_file():
         contents = settings.github_event_path.read_text()
-        print("contents:")
-        print(contents)
-    logging.info("Debugging")
-    raise RuntimeError("Debugging")
-
-    for issue in repo.get_issues(state="open"):
-        process_issue(issue=issue, settings=settings, owner=owner)
+        github_event = PartialGitHubEvent.parse_raw(contents)
+    if (
+        settings.github_event_name == "issues"
+        or settings.github_event_name == "issue_comment"
+    ):
+        if github_event and github_event.issue:
+            issue = repo.get_issue(github_event.issue.number)
+            if issue.state == "open":
+                process_issue(issue=issue, settings=settings, owner=owner)
+    else:
+        for issue in repo.get_issues(state="open"):
+            process_issue(issue=issue, settings=settings, owner=owner)
     logging.info(f"Finished")
