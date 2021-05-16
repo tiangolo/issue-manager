076a3b90a273b6bb2ececc53b2f2c3f7af03e988
Sebastián Ramírez <tiangolo@gmail.com>
2021-05-16 20:12:54 +0200

    ✨ Add support for managing PRs and remove support for HTML comments to avoid rate limits (#12)
    * 🔥 Remove support for HTML comments to avoid rate limits
    
    and because that feature was not used much after having support for labels
    
    * 🔊 Add not closing logging
    
    * 📌 Pin ranges of PyGitHub and pydantic better
    
    * ✨ Enable processing PRs
    
    * ✨ Add support for reading PR event data
    
    * 📝 Update docs supporting PRs
    
    * 📝 Update README with new version

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.7
 
-RUN pip install PyGithub "pydantic==1.5.1"
+RUN pip install "PyGithub>=1.55,<2.0" "pydantic>=v1.8.2,<2.0"
 
 COPY ./app /app
 
--- app/main.py
+++ app/main.py
@@ -7,14 +7,12 @@ from github import Github
 from github.Issue import Issue
 from github.IssueComment import IssueComment
 from github.IssueEvent import IssueEvent
-from github.NamedUser import NamedUser
 from pydantic import BaseModel, BaseSettings, SecretStr, validator
 
 
 class KeywordMeta(BaseModel):
     delay: timedelta = timedelta(days=10)
-    users: List[str] = []
-    message: str = "Assuming the original issue was solved, it will be automatically closed now."
+    message: str = "Assuming the original need was handled, this will be automatically closed now."
     remove_label_on_comment: bool = True
     remove_label_on_close: bool = False
 
@@ -39,6 +37,7 @@ class PartialGitHubEventIssue(BaseModel):
 
 class PartialGitHubEvent(BaseModel):
     issue: Optional[PartialGitHubEventIssue] = None
+    pull_request: Optional[PartialGitHubEventIssue] = None
 
 
 def get_last_comment(issue: Issue) -> Optional[IssueComment]:
@@ -87,7 +86,7 @@ def close_issue(
             issue.remove_from_labels(keyword)
 
 
-def process_issue(*, issue: Issue, settings: Settings, owner: NamedUser) -> None:
+def process_issue(*, issue: Issue, settings: Settings) -> None:
     logging.info(f"Processing issue: #{issue.number}")
     label_strs = set([label.name for label in issue.get_labels()])
     events = list(issue.get_events())
@@ -95,12 +94,10 @@ def process_issue(*, issue: Issue, settings: Settings, owner: NamedUser) -> None
     last_comment = get_last_comment(issue)
     for keyword, keyword_meta in settings.input_config.items():
         # Check closable delay, if enough time passed and the issue could be closed
-        closable_delay = False
-        if (
+        closable_delay = (
             last_comment is None
             or (datetime.utcnow() - keyword_meta.delay) > last_comment.created_at
-        ):
-            closable_delay = True
+        )
         # Check label, optionally removing it if there's a comment after adding it
         if keyword in label_strs:
             logging.info(f'Keyword: "{keyword}" in issue labels')
@@ -127,24 +124,10 @@ def process_issue(*, issue: Issue, settings: Settings, owner: NamedUser) -> None
                     label_strs=label_strs,
                 )
                 break
-        # Check HTML comments by allowed users
-        if (
-            last_comment
-            and f"<!-- issue-manager: {keyword} -->" in last_comment.body
-            and closable_delay
-            and last_comment.user.login in keyword_meta.users + [owner.login]
-        ):
-            logging.info(
-                f'Last comment by user: "{last_comment.user.login}" had HTML keyword '
-                f'comment: "{keyword}" and there\'s a closable delay.'
-            )
-            close_issue(
-                issue=issue,
-                keyword_meta=keyword_meta,
-                keyword=keyword,
-                label_strs=label_strs,
-            )
-            break
+            else:
+                logging.info(
+                    f"Not clossing issue: #{issue.number} as the delay hasn't been reached: {keyword_meta.delay}"
+                )
 
 
 if __name__ == "__main__":
@@ -153,20 +136,27 @@ if __name__ == "__main__":
     logging.info(f"Using config: {settings.json()}")
     g = Github(settings.input_token.get_secret_value())
     repo = g.get_repo(settings.github_repository)
-    owner: NamedUser = repo.owner
     github_event: Optional[PartialGitHubEvent] = None
     if settings.github_event_path.is_file():
         contents = settings.github_event_path.read_text()
         github_event = PartialGitHubEvent.parse_raw(contents)
     if (
         settings.github_event_name == "issues"
+        or settings.github_event_name == "pull_request_target"
         or settings.github_event_name == "issue_comment"
     ):
-        if github_event and github_event.issue:
-            issue = repo.get_issue(github_event.issue.number)
-            if issue.state == "open":
-                process_issue(issue=issue, settings=settings, owner=owner)
+        if github_event:
+            issue_number: Optional[int] = None
+            if github_event.issue:
+                issue_number = github_event.issue.number
+            elif github_event.pull_request:
+                issue_number = github_event.pull_request.number
+            if issue_number is not None:
+                issue = repo.get_issue(issue_number)
+                if issue.state == "open":
+                    process_issue(issue=issue, settings=settings)
     else:
-        for issue in repo.get_issues(state="open"):
-            process_issue(issue=issue, settings=settings, owner=owner)
+        for keyword, keyword_meta in settings.input_config.items():
+            for issue in repo.get_issues(state="open", labels=[keyword]):
+                process_issue(issue=issue, settings=settings)
     logging.info("Finished")
